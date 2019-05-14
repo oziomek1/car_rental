3ef70a97d1481c0ff6d752fc209a09371a2caa06
oziomek1 <ozimekwojciech@zoho.eu>
2019-05-14 21:48:37 +0200

    add rental routes and car images

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Car rental' });
+  var today = new Date();
+  var tommorow = new Date();
+  tommorow.setDate(new Date().getDate() + 1);
+  res.render('index', {
+    title: 'Rent a car',
+    date_from: today.toISOString().slice(0, 10),
+    date_to: tommorow.toISOString().slice(0, 10),
+  });
 });
 
 module.exports = router;
--- backend/routes/rental.js
+++ backend/routes/rental.js
@@ -1,32 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* POST rental check */
-router.post('/check', function(req, res, next) {
-  var carName = req.body.car;
-  var dateStart = req.body.date_start;
-  var dateEnd = req.body.date_end;
-
+/* POST rental */
+router.post('/', function(req, res, next) {
+  var dateFrom = req.body.date_from;
+  var dateTo = req.body.date_to;
   res.render('rental', {
-    title: 'Checking rental availability',
-    carName: carName,
-    dateStart: dateStart,
-    dateEnd: dateEnd,
+    title: 'Rent a car - details',
+    dateFrom: dateFrom,
+    dateTo: dateTo,
   });
 });
 
 /* GET rental */
 router.get('/', function(req, res, next) {
-  var carName = 'Test';
-  var dateStart = 'test';
-  var dateEnd = 'test';
-
+  var today = new Date();
+  var tommorow = new Date();
+  tommorow.setDate(new Date().getDate() + 1);
   res.render('rental', {
-    title: 'Showing example rental availability',
-    carName: carName,
-    dateStart: dateStart,
-    dateEnd: dateEnd,
+    title: 'Rent a car',
+    dateFrom: today.toISOString().slice(0, 10),
+    dateTo: tommorow.toISOString().slice(0, 10),
   });
 });
+
+/* GET RENTAL FIND CARS */
 
 module.exports = router;
